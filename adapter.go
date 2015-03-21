de170880f3b0c9bae92d62a0fd775facff615eac
aquilax <aquilax@gmail.com>
2015-03-21 15:08:15 +0100

    Initial commit

---
+++ adapter.go
@@ -0,0 +1,28 @@
+package dummy
+
+import (
+	"github.com/Townsita/townsita"
+)
+
+type Dummy struct{}
+
+func New() *Dummy {
+	return &Dummy{}
+}
+
+func (d *Dummy) Init() {
+}
+
+func (d *Dummy) MustGetMessageTypes() []townsita.MessageType {
+	return []townsita.MessageType{
+		townsita.MessageType{"1", "Message Type 1"},
+		townsita.MessageType{"2", "Message Type 1"},
+	}
+}
+
+func MustGetMessageSubTypes(messageType int) []townsita.MessageType {
+	return []townsita.MessageType{
+		townsita.MessageType{"1", "Message Type 1"},
+		townsita.MessageType{"2", "Message Type 1"},
+	}
+}
